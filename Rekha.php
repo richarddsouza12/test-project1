253683da470a0fd33cf28bed100fec5a816f8266
Palash Gupta <palashgupta817@gmail.com>
2020-05-01 12:01:56 +0530

    added by palash gupta

--- Rekha.php
+++ Rekha.php
@@ -1,6 +1,5 @@
 // Hi This is Rekha
 <?php
-<<<<<<< HEAD
  class utils{
 
  	public function add() {
@@ -8,18 +7,3 @@
     }
  }
 ?>
-=======
-
-class Utils{
-
-	public function add(){
-		return (1 + 3); //changes done by shubham
-		return ( 4 + 4 );   //changes by richard
-	}
-
-
-}
-
-
-?>
->>>>>>> 863250371d25cdd9b5bd551d2a27902a0b1baa70
