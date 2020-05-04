862fdd48ed22cf0b7c051c8bbdb27947da720883
Ajay Khedkar <ajaykhedkar7210@gmail.com>
2020-05-04 17:00:05 +0530

    Moving all files to a separate directory

---
+++ Practice-via-terminal/Employee.php
@@ -0,0 +1,19 @@
+<?php
+
+class Employee {
+
+	public function f1(){}
+	public function f2(){}
+	public function f3(){}
+	public function f4(){}
+
+	public function f5(){
+		echo "f5";
+	}
+	public function f6(){
+		$x = new Utils();
+	}
+}
+
+
+?>
