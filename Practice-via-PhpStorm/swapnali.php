079cad6e69909027ce04168f7655a800b76f7782
swapnalis <swapnalis2208@gmail.com>
2020-05-08 17:04:35 +0530

    some maths

--- Practice-via-PhpStorm/swapnali.php
+++ Practice-via-PhpStorm/swapnali.php
@@ -7,11 +7,10 @@ echo "1+1+1+1";
 
 function addddd()
 {
-    echo "here some stuff";
-   echo "more stuff";
-    echo "hererererer";
-    echo "swapnali shinde";
-    echo "sentence from phpstorm";
-    echo "here";
+    $m = 18;
+    if($m % 2 == 0)
+    {
+        echo "Even";
+    }
 }
 ?>
