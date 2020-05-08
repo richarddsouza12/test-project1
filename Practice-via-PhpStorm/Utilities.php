f7f85e822793a8b25575cde98ba895a369850d49
Richard Dsouza <richarddsouza12@gmail.com>
2020-05-08 17:00:49 +0530

    Added Utils

---
+++ Practice-via-PhpStorm/Utilities.php
@@ -0,0 +1,43 @@
+<?php
+
+class Utilities{
+
+	public function f1(){
+		while(true){
+
+			if(true){
+
+			}else
+			{
+				echo "1";
+			}
+			break;
+		}
+	}
+
+	public function f2(){
+
+		echo "f2";
+	}
+
+	public function f3(){
+
+		echo "f3";
+	}
+
+	public function f4(){
+
+		echo "f4";
+	}
+
+	public function f5(){
+		echo "f4";
+	}
+
+	public function f6(){
+		echo "f4";
+	}
+	public function f7(){
+		echo "f4";
+	}
+}
