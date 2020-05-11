390bd6f09362feb3f3bc657d050f14fbc5f8793d
Rekha Kawalkar <rekha.ytl@gmail.com>
2020-05-11 17:28:01 +0530

    Added class definition

--- Practice-via-PhpStorm/Rekha.php
+++ Practice-via-PhpStorm/Rekha.php
@@ -2,4 +2,10 @@
 echo 'This is my file';
 echo 'updating the file';
 echo 'merge New Feature branch';
+
+class Rekha {
+	public function demoFunction(){
+		echo 'Added new class and merging it on master branch';
+	}
+}
 ?>
